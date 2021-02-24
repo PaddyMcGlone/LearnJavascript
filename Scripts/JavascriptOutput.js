b28d4607dadf05dc2a729690bc3d5c48c02990f1
Paddy McGlone <paddymcglone@gmail.com>
2021-02-24 19:48:18 +0000

    Adding Javscript part 2

---
+++ Scripts/JavascriptOutput.js
@@ -0,0 +1,15 @@
+// document.getElementById("demo").innerHTML = 5 + 6;
+
+// document.write(5+6);
+
+// window.alert(5+6);
+
+// console.log(5+5);
+
+var x, y, z;
+x = 5;
+y = 6;
+
+z = x + y;
+
+document.getElementById("demo").innerHTML = "The total is: " + z;
